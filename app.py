bb7ed3da56a1fac32b6b75153069fe1bef777c64
kommaraju nitish sai <kommaraju.nitish.9@gmail.com>
2020-03-22 22:42:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import Indiacorona.py
 app = Flask(__name__)
 @app.route('/, methods=['GET'])
 def tot():
-    return jsonify(Indiacorona.py .f)
+    return jsonify(Indiacorona.f)
 @app.route('/stats', methods=['GET'])
 def index():
-    return jsonify(Indiacorona.py.d)
+    return jsonify(Indiacorona.d)
 if __name__=="__main__":
     app.run(threaded=True, port=5000)
